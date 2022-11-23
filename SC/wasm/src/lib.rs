12218eac77b412aef57ede0979f6eb1f68e6d08c
porxyz <29072624+porxyz@users.noreply.github.com>
2022-11-23 13:36:40 -0500

    Create lib.rs

---
+++ SC/wasm/src/lib.rs
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    egldvoting
+    (
+        getEndTime
+        getMaxAllowedParticipants
+        getParticipantAddrByName
+        getParticipantInfo
+        getParticipants
+        getRegisteringEndTime
+        getRegisteringTax
+        getVoteCount
+        getVotingTax
+        register_participant
+        transfer_funds_after_voting
+        vote_participant
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
